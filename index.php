4d4ec6b44bfd3f129ab846fea1c3b8ec50ec1f4d
Hiyu Yamauchi <130884957+hiyu15123@users.noreply.github.com>
2025-09-15 23:50:14 +0900

    add : headerを追加
    headerを追加しました。

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+   <header class="header blContainer">
+		<h1 class="header-logo">
+			<a href="/">
+				<img src="/img/logo.svg" alt="デモサイト株式会社">
+			</a>
+		</h1>
+		<nav class="header-menu mainMenu">
+			<ul class="mainMenu-list">
+				<li class="mainMenu-item">
+					<a class="mainMenu-link" href="">ホーム</a>
+				</li>
+				<li class="mainMenu-item">
+					<a class="mainMenu-link" href="">お知らせ</a>
+				</li>
+				<li class="mainMenu-item">
+					<a class="mainMenu-link" href="">サービス</a>
+				</li>
+				<li class="mainMenu-item">
+					<a class="mainMenu-link" href="">施工事例</a>
+				</li>
+				<li class="mainMenu-item">
+					<a class="mainMenu-link" href="">会社概要</a>
+				</li>
+				<li class="mainMenu-item">
+					<a class="mainMenu-link" href="">お問い合わせ</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+   <div class="keyVisual">
+			<div class="keyVisual-inner blContainer">
+         <img src="/img/header.jpg" alt="">
+      </div>
+   </div>
+</body>
+</html>
