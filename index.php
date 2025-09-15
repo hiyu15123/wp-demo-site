4f4a8e0c14d483f6f97254fffbca3d4569333e3c
Hiyu Yamauchi <130884957+hiyu15123@users.noreply.github.com>
2025-09-16 00:43:55 +0900

    upgrade : WP関数を記述
    href、src属性をWP関数に記述し直しました。

--- index.php
+++ index.php
@@ -3,40 +3,41 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="<?php echo get_theme_file_uri('/assets/css/style.css'); ?>">
 </head>
 <body>
    <header class="header blContainer">
 		<h1 class="header-logo">
-			<a href="/">
-				<img src="/img/logo.svg" alt="デモサイト株式会社">
+			<a href="<?php echo home_url('/'); ?>">
+				<img src="<?php echo get_theme_file_uri('/img/logo.svg'); ?>" alt="デモサイト株式会社">
 			</a>
 		</h1>
 		<nav class="header-menu mainMenu">
 			<ul class="mainMenu-list">
 				<li class="mainMenu-item">
-					<a class="mainMenu-link" href="">ホーム</a>
+					<a class="mainMenu-link" href="<?php echo home_url('/') ?>">ホーム</a>
 				</li>
 				<li class="mainMenu-item">
-					<a class="mainMenu-link" href="">お知らせ</a>
+					<a class="mainMenu-link" href="<?php echo home_url('/news/') ?>">お知らせ</a>
 				</li>
 				<li class="mainMenu-item">
-					<a class="mainMenu-link" href="">サービス</a>
+					<a class="mainMenu-link" href="<?php echo home_url('/service/') ?>">サービス</a>
 				</li>
 				<li class="mainMenu-item">
-					<a class="mainMenu-link" href="">施工事例</a>
+					<a class="mainMenu-link" href="<?php echo home_url('/works/') ?>">施工事例</a>
 				</li>
 				<li class="mainMenu-item">
-					<a class="mainMenu-link" href="">会社概要</a>
+					<a class="mainMenu-link" href="<?php echo home_url('/company/') ?>">会社概要</a>
 				</li>
 				<li class="mainMenu-item">
-					<a class="mainMenu-link" href="">お問い合わせ</a>
+					<a class="mainMenu-link" href="<?php echo home_url('/contact/') ?>">お問い合わせ</a>
 				</li>
 			</ul>
 		</nav>
 	</header>
    <div class="keyVisual">
 			<div class="keyVisual-inner blContainer">
-         <img src="/img/header.jpg" alt="">
+         <img src="<?php echo get_theme_file_uri('/assets/img/header.jpg'); ?>" alt="">
       </div>
    </div>
 </body>
