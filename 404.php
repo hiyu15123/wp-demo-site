7f47a5444257952d1313238e597f9f791795b671
Hiyu Yamauchi <130884957+hiyu15123@users.noreply.github.com>
2025-09-17 16:56:43 +0900

    add : 404.phpを作成。
    404.phpを作成しました。

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+   <div class="page404 blContainer">
+      <div class="page404-heading">Not Found</div>
+      <p class="page404-message">
+         申し訳ございませんが、指定されたページが存在しないか、移動または削除された可能性があります。<br>
+         ホームページに戻るか、メニューから他のページをご覧ください。
+      </p>
+   </div>
+<?php get_footer(); ?>
