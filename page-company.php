a93e40ceb8c7997f271793b11faeca5b1db93c93
Hiyu Yamauchi <130884957+hiyu15123@users.noreply.github.com>
2025-09-17 16:34:48 +0900

    add : page-company、page-serivce.phpを作成。　
    page-company.phpとpage-serivce.phpを作成しました。

---
+++ page-company.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+<div class="blContainer">
+   <table class="table">
+		<tr class="table-row">
+			<th class="table-header">会社名</th>
+			<td class="table-data">アルカディアストラクチャー株式会社</td>
+		</tr>
+		<tr class="table-row">
+			<th class="table-header">所在地</th>
+			<td class="table-data">〒160-0000<br>東京都新宿区新宿1丁目1-1</td>
+		</tr>
+		<tr class="table-row">
+			<th class="table-header">電話番号</th>
+			<td class="table-data">03-1234-5678</td>
+		</tr>
+		<tr class="table-row">
+			<th class="table-header">役員</th>
+			<td class="table-data">代表取締役　頷 太郎</td>
+		</tr>
+		<tr class="table-row">
+			<th class="table-header">設立</th>
+			<td class="table-data">2016年 8月22日</td>
+		</tr>
+		<tr class="table-row">
+			<th class="table-header">取引銀行</th>
+			<td class="table-data">三井住友銀行、みずほ銀行、三菱ＵＦJ銀行</td>
+		</tr>
+		<tr class="table-row">
+			<th class="table-header">事業概要</th>
+			<td class="table-data">
+				<ul>
+					<li>モダンデザイン住宅の設計・施工</li>
+					<li>エコフレンドリーな住居空間の提案（太陽光発電や断熱材利用）</li>
+					<li>商業施設の設計・施工</li>
+					<li>空間を活かしたブランド価値向上のための内装デザイン</li>
+				</ul>
+			</td>
+		</tr>
+	</table>
+</div>
+<?php get_footer(); ?>
---
+++ page-service.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+<div class="service blContainer">
+   サービスページの内容が入ります。
+</div>
+<?php get_footer(); ?>
